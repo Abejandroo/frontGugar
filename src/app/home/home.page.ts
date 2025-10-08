15381f65f8da444abc03c96bb267fb8ed9966247
oshersa-170701 <luxfer1771@gmail.com>
2025-10-07 21:55:27 -0600

    HTML DE LA PAGINA HOME

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCol, IonRow, IonGrid, IonHeader, IonToolbar, IonTitle, IonContent,RouterModule],
 })
 export class HomePage {
   constructor() {}
