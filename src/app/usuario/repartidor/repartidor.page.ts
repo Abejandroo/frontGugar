c61a2a4a8da5d084d91f499fbf11c30e7d16136b
oshersa-170701 <oshersa1771@gmail.com>
2025-10-16 19:26:32 -0600

    diseño en paginas repartidor y supervisor

--- src/app/usuario/repartidor/repartidor.page.ts
+++ src/app/usuario/repartidor/repartidor.page.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { menu } from 'ionicons/icons';
 
 @Component({
   selector: 'app-repartidor',
   templateUrl: './repartidor.page.html',
   styleUrls: ['./repartidor.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class RepartidorPage implements OnInit {
+export class RepartidorPage {
+   constructor(private readonly router: Router,
+  //private readonly authService: AuthService,
+  //private readonly menu: MenuController,
 
-  constructor() { }
+  ) { 
+   addIcons({
+  menu,
+});
+  }
+  logout() {
+  //  this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+  toggleMenu() {
+   // this.menu.toggle();
+  }
 
-  ngOnInit() {
+  irATutores() {
+    this.router.navigate(['/tutores/formulario']);
+  }
+
+  irAAlumnos() {
+    this.router.navigate(['/alumnos/formulario']);
+  }
+
+  irAGrupos() {
+    this.router.navigate(['/grupos']);
+  }
+
+  irAMaestros() {
+    this.router.navigate(['/maestros/registro']);
   }
 
 }
--- src/app/usuario/supervisor/supervisor.page.ts
+++ src/app/usuario/supervisor/supervisor.page.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { menu } from 'ionicons/icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-supervisor',
   templateUrl: './supervisor.page.html',
   styleUrls: ['./supervisor.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonToolbar, CommonModule, FormsModule]
 })
-export class SupervisorPage implements OnInit {
+export class SupervisorPage  {
+   constructor(private readonly router: Router,
+  //private readonly authService: AuthService,
+  //private readonly menu: MenuController,
 
-  constructor() { }
+  ) { 
+   addIcons({
+  menu,
+});
+  }
+  logout() {
+  //  this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+  toggleMenu() {
+   // this.menu.toggle();
+  }
 
-  ngOnInit() {
+  irATutores() {
+    this.router.navigate(['/tutores/formulario']);
+  }
+
+  irAAlumnos() {
+    this.router.navigate(['/alumnos/formulario']);
+  }
+
+  irAGrupos() {
+    this.router.navigate(['/grupos']);
+  }
+
+  irAMaestros() {
+    this.router.navigate(['/maestros/registro']);
   }
 
 }
