3f2bff5a39e77ec57a053d917708965eaff1d9c3
oshersa-170701 <oshersa1771@gmail.com>
2025-11-17 15:50:27 -0600

    CAMBIO DE DIRECCIONES EN PAGINAS DE REPARTIDOR Y SUPERVISOR DESPUES DEL INICIO SE SESION, DE ESTOS MISMOS

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -17,13 +17,13 @@ import { Auth } from 'src/app/service/auth';
 export class LoginPage {
   correo: string = '';
   contrasena: string = '';
-  rolSeleccionado: string = ''; // El rol que viene del botón (query param)
+  rolSeleccionado: string = '';
   mostrarContrasena: boolean = false;
 
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute,
-    private readonly authService: Auth, // Inyectamos el servicio
+    private readonly authService: Auth, 
     private readonly toastController: ToastController
   ) {
     this.route.queryParams.subscribe(params => {
@@ -37,32 +37,22 @@ export class LoginPage {
       this.mostrarToast('Por favor completa todos los campos', 'warning');
       return;
     }
-
-    // Llamamos al servicio único (ya no necesitamos if/else por rol aquí)
     this.authService.login(this.correo, this.contrasena).subscribe({
       next: (res: any) => {
         const usuarioBD = res.user;
-        
-        // VALIDACIÓN DE SEGURIDAD 🛡️
-        // Verificamos si el rol de la BD coincide con el botón que presionó
-        // Nota: Asegúrate que en BD los roles sean 'admin', 'repartidor', etc. (o mapealos)
         if (usuarioBD.role === this.rolSeleccionado) {
           
           this.mostrarToast(`Bienvenido, ${usuarioBD.name}`, 'success');
-          
-          // Redirección según el rol
           if (this.rolSeleccionado === 'admin') {
             this.router.navigate(['/usuario/admin']);
           } else if (this.rolSeleccionado === 'supervisor') {
-            this.router.navigate(['/supervisor']);
+            this.router.navigate(['/usuario/supervisor']);
           } else if (this.rolSeleccionado === 'repartidor') {
-            this.router.navigate(['/repartidor']);
+            this.router.navigate(['/usuario/repartidor']);
           }
 
         } else {
-          // Si intenta entrar como Admin siendo Repartidor
           this.mostrarToast('No tienes permisos para acceder en esta sección', 'danger');
-          // Opcional: Limpiar storage si falló la validación de rol
           this.authService.logout();
         }
       },
